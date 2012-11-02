90f13b2381829fc5c00537352c45b56539e094d3
Mathias <Mathias@147.87.242.171>
2012-11-02 10:37:05 +0100

    Initial commit

---
+++ SpaceWars/src/spacewars/Launcher.java
@@ -0,0 +1,15 @@
+package spacewars;
+
+public class Launcher
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}

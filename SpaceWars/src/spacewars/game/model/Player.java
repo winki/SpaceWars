90d07af21e0395f169121789d452ed4d6fca7ae1
boscka <boscka@FLA1623.comet.ch>
2012-11-09 10:34:10 +0100

    add getScore to player class

--- SpaceWars/src/spacewars/game/model/Player.java
+++ SpaceWars/src/spacewars/game/model/Player.java
@@ -2,9 +2,14 @@ package spacewars.game.model;
 
 public class Player
 {   
-    public int score;
+    public int score = 0;
     //amount of res he owns
     public int res;
     //which homeplanet does he have
     public int homePlanetNr;
+    
+    
+    public int getScore(){
+    	return this.score;
+    }
 }

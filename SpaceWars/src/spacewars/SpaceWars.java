6c89717dceb646ee790cd4d40c8878cda271144a
kai@comet <boschung91@gmail.com>
2012-12-08 11:17:19 +0100

    goooood

--- SpaceWars/src/spacewars/SpaceWars.java
+++ SpaceWars/src/spacewars/SpaceWars.java
@@ -33,16 +33,16 @@ public class SpaceWars
       
       runServer();  
       
-      try
-      {
-         Thread.sleep(500);
-      }
-      catch (InterruptedException e)
-      {
-         e.printStackTrace();         
-      }
-      
-      runClient("localhost");
+//      try
+//      {
+//         Thread.sleep(500);
+//      }
+//      catch (InterruptedException e)
+//      {
+//         e.printStackTrace();         
+//      }
+//      
+//      runClient("localhost");
       
       // run game
       //SpaceWarsGame.getInstance().run();
@@ -110,7 +110,8 @@ public class SpaceWars
       Network.bindServer(server);      
       
       // run server in his own thread
-      new Thread(server).start();
+      //new Thread(server).start();
+      server.run();
    }
    
    /**
